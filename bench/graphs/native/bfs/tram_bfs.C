b73e7b92fe84480cc8923be99fd2f7a53f823dab
frolo <frolo@da144297-cd6b-48be-b417-ff744a2206ef>
2016-09-06 10:53:21 +0000

    tram_bfs_radix renamed to tram_bfs

--- bench/graphs/native/bfs/tram_bfs.C
+++ bench/graphs/native/bfs/tram_bfs.C
@@ -1,25 +1,28 @@
-//#define CMK_TRAM_VERBOSE_OUTPUT
-#include "NDMeshStreamer.h"
-#include "GraphLib.h"
-#include "common.h"
+#include <GraphLib.h>
+#include <common.h>
 
 class BFSVertex;
 class BFSEdge;
 class	BFSGraph;
 
+#define RADIX_ENABLED
+//#define RADIX 1
+
 typedef struct __dtype {
-	int level; 
+	int level;
 	CmiUInt8 parent; 
+	int r;
+  __dtype(int level, CmiUInt8 parent, int r) : 
+		level(level), parent(parent), r(r) {}
 	__dtype() {}
-  __dtype(int level, CmiUInt8 parent) : 
-		level(level), parent(parent) {}
 	void pup(PUP::er & p) {
 		p | level;
 		p | parent;
+		p | r;
 	}
 } dtype;
 
-#include "tram_bfs.decl.h"
+#include "tram_bfs_radix.decl.h"
 
 CmiUInt8 N, M;
 int K = 16;
@@ -34,6 +37,7 @@ class BFSGraph : public GraphLib::Graph<
 	BFSEdge,
 	CProxy_BFSVertex,
 	GraphLib::TransportType::Charm> {
+
 public:
 	BFSGraph() : 
 		GraphLib::Graph<
@@ -47,7 +51,8 @@ public:
 				BFSVertex, 
 				BFSEdge,
 				CProxy_BFSVertex, 
-				GraphLib::TransportType::Charm >(g) {
+				GraphLib::TransportType::Charm >(g)	{
+		//masterProxy = CProxy_Master::ckNew(g); 
 
 		int dims[2] = {CkNumNodes(), CkNumPes() / CkNumNodes()};
 		CkPrintf("Aggregation topology: %d %d\n", dims[0], dims[1]);
@@ -86,74 +91,92 @@ struct BFSEdge {
 };
 
 class BFSVertex : public CBase_BFSVertex {
-private:
-	std::vector<BFSEdge> adjlist;
-	int level;
-	CmiUInt8 parent;
+	private:
+		std::vector<BFSEdge> adjlist;
+		enum State {White, Gray, Black} state;
+		CmiUInt8 parent;
+		int level;
+		CmiUInt8 numScannedEdges;
 
-public:
-  BFSVertex() : level(-1), parent(-1) {
-
-    // Contribute to a reduction to signal the end of the setup phase
-    //contribute(CkCallback(CkReductionTarget(TestDriver, start), driverProxy));
-  }
-
-	void connectVertex(const BFSEdge & edge) {
-		adjlist.push_back(edge);
-	}
-
-	void process(const BFSEdge & edge) {
-		connectVertex(edge);
-	}
-
-  BFSVertex(CkMigrateMessage *msg) {}
-
-  inline void process(const dtype  &data) {
-		const int & level = data.level;
-		const CmiUInt8 & parent = data.parent;
-		if ((this->level >= 0) && (this->level <= level + 1))
-			return;
-		//CkPrintf("%d: updated\n", thisIndex);
-		this->level = level + 1;
-		this->parent = parent;
-		typedef typename std::vector<BFSEdge>::iterator Iterator; 
-    ArrayMeshStreamer<dtype, long long, BFSVertex, SimpleMeshRouter>
-      * localAggregator = aggregator.ckLocalBranch();
-
-		for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-			localAggregator->insertData(dtype(this->level, thisIndex), it->v);
+	public:
+		BFSVertex() : state(White), level(-1), parent(-1), numScannedEdges(0) {
+			// Contribute to a reduction to signal the end of the setup phase
+			//contribute(CkCallback(CkReductionTarget(TestDriver, start), driverProxy));
 		}
-		//localAggregator->done();
-  }
 
-  void make_root() {
-		if (level >= 0)
-			return;
-		//CkPrintf("%d: updated\n", thisIndex);
-		level = 0;
-    ArrayMeshStreamer<dtype, long long, BFSVertex, SimpleMeshRouter>
-      * localAggregator = aggregator.ckLocalBranch();
-
-		typedef typename std::vector<BFSEdge>::iterator Iterator; 
-		for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-      localAggregator->insertData(dtype(this->level, thisIndex), it->v);
+		void connectVertex(const BFSEdge & edge) {
+			if (edge.v > CkNumPes() * N) 
+				CkAbort("Incorrect dest vertex ID");
+			adjlist.push_back(edge);
 		}
-  }
 
-	void getScannedVertexNum() {
-		CmiUInt8 c = (parent == -1 ? 0 : 1);
-		contribute(sizeof(CmiUInt8), &c, CkReduction::sum_long,
-				CkCallback(CkReductionTarget(TestDriver, done), driverProxy));
-	}
+		void process(const BFSEdge & edge) {
+			connectVertex(edge);
+		}
 
-	void verify() {
-		if ((parent != -1) && (parent != thisIndex))
-			thisProxy[parent].check(level);
-	}
-	void check(int level) {
-		CkAssert(this->level + 1 == level);
-	}
+		BFSVertex(CkMigrateMessage *msg) {}
+
+		void make_root() {
+			if (level >= 0)
+				return;
+			level = 0;
+			state = Black;
+			parent = thisIndex;
+			ArrayMeshStreamer<dtype, long long, BFSVertex, SimpleMeshRouter>
+				* localAggregator = aggregator.ckLocalBranch();
+
+			typedef typename std::vector<BFSEdge>::iterator Iterator; 
+			for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
+				localAggregator->insertData(dtype(this->level, thisIndex, RADIX), it->v);
+				numScannedEdges++;
+			}
+		}
+
+		void update(int level, CmiUInt8 parent, int r) {
+
+			if ((this->level >= 0) && (this->level <= level + 1))
+				return;
+			//CkPrintf("%d (pe=%d): updated, radius %d\n", thisIndex, getuChareSet()->getPe(), r);
+			this->level = level + 1;
+			this->parent = parent;
+
+			if (r > 0) {
+				state = Black;
+				ArrayMeshStreamer<dtype, long long, BFSVertex, SimpleMeshRouter>
+					* localAggregator = aggregator.ckLocalBranch();
+				typedef typename std::vector<BFSEdge>::iterator Iterator; 
+				for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
+					localAggregator->insertData(dtype(this->level, thisIndex, r - 1), it->v);
+				}
+			} else {
+				state = Gray;
+				typedef typename std::vector<BFSEdge>::iterator Iterator; 
+				for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
+					thisProxy[it->v].update(this->level, thisIndex, RADIX);
+				}
+				//driverProxy.grayVertexExist();
+			}
+		}
+
+		void process(const dtype &data) {
+			update(data.level, data.parent, data.r);
+		}
+
+		void getScannedVertexNum() {
+			CmiUInt8 c = (parent == -1 ? 0 : 1);
+			contribute(sizeof(CmiUInt8), &c, CkReduction::sum_long,
+					CkCallback(CkReductionTarget(TestDriver, done), driverProxy));
+		}
+
+		void verify() {
+			if ((parent != -1) && (parent != thisIndex))
+				thisProxy[parent].check(level);
+		}
+
+		void check(int level) {
+			CkAssert(this->level + 1 == level);
+		}
 };
 
 #include "driver.C"
-#include "tram_bfs.def.h"
+#include "tram_bfs_radix.def.h"
