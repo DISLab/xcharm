abe44bafb5955849a2d8c0b85a41730846ae3452
frolo <frolo@da144297-cd6b-48be-b417-ff744a2206ef>
2016-09-08 14:57:20 +0000

    cc refactoring (tram_cc updated)

--- bench/graphs/native/cc/tram_cc.C
+++ bench/graphs/native/cc/tram_cc.C
@@ -1,252 +1,168 @@
-//#define CMK_TRAM_VERBOSE_OUTPUT
-#include "NDMeshStreamer.h"
-#include <GraphGenerator.h>
+#include <GraphLib.h>
 #include <common.h>
-#include <sstream>
 
-#define RADIUS
-#define R 100
+class CCVertex;
+class CCEdge;
+class	CCGraph;
+
+#define RADIX_ENABLED
+//#define RADIX 1
 
 typedef struct __dtype {
-	CmiUInt8 c;
-#if defined (RADIUS)
+	CmiUInt8 c; 
 	int r;
-	__dtype (CmiUInt8 c, int r) : c(c), r(r) {}
-#else
-	__dtype (CmiUInt8 c) : c(c) {}
-#endif
+  __dtype(CmiUInt8 c, int r) : 
+		c(c), r(r) {}
 	__dtype() {}
-	void pup(PUP::er & p) { 
+	void pup(PUP::er & p) {
 		p | c;
-#if defined (RADIUS)
 		p | r;
-#endif
 	}
 } dtype;
+
 #include "tram_cc.decl.h"
 
+CmiUInt8 N, M;
+int K = 16;
 CProxy_TestDriver driverProxy;
-CProxy_ArrayMeshStreamer<dtype, int, CCVertex,
+CProxy_ArrayMeshStreamer<dtype, long long, CCVertex,
                          SimpleMeshRouter> aggregator;
 // Max number of keys buffered by communication library
 const int numMsgsBuffered = 1024;
 
+class CCGraph : public GraphLib::Graph<
+	CCVertex,
+	CCEdge,
+	CProxy_CCVertex,
+	GraphLib::TransportType::Charm> {
+
+public:
+	CCGraph() : 
+		GraphLib::Graph<
+				CCVertex, 
+				CCEdge,
+				CProxy_CCVertex, 
+				GraphLib::TransportType::Charm >()	
+		{}
+
+	CCGraph(CmiUInt8 nVertex) : 
+		GraphLib::Graph<
+				CCVertex, 
+				CCEdge,
+				CProxy_CCVertex, 
+				GraphLib::TransportType::Charm >(nVertex) {
+		int dims[2] = {CkNumNodes(), CkNumPes() / CkNumNodes()};
+		CkPrintf("Aggregation topology: %d %d\n", dims[0], dims[1]);
+		// Instantiate communication library group with a handle to the client
+		aggregator =
+			CProxy_ArrayMeshStreamer<dtype, long long, CCVertex, SimpleMeshRouter>
+			::ckNew(numMsgsBuffered, 2, dims, g, 1);
+	}
+	void start() {
+    CkCallback startCb(CkIndex_CCVertex::start(), g);
+    CkCallback endCb(CkIndex_TestDriver::startVerificationPhase(), driverProxy);
+    aggregator.init(g.ckGetArrayID(), startCb, endCb, -1, true);
+	}
+
+	void getScannedVertexNum() {
+		g.getScannedVertexNum();
+	}
+	void verify() {
+		g.verify();
+	}
+};
+
 struct CCEdge {
 	CmiUInt8 v;
-	double w;
 
 	CCEdge() {}
-	CCEdge(CmiUInt8 v, double w) : v(v), w(w) {}
+	CCEdge(CmiUInt8 v, double w) : v(v) {}
 	void pup(PUP::er &p) { 
 		p | v; 
-		p | w;
 	}
 };
 
 class CCVertex : public CBase_CCVertex {
-private:
-	std::vector<CCEdge> adjlist;
-	CmiUInt8 componentId;
+	private:
+		std::vector<CCEdge> adjlist;
+		CmiUInt8 componentId;
 
-public:
-  CCVertex() {
-    // Contribute to a reduction to signal the end of the setup phase
-    //contribute(CkCallback(CkReductionTarget(TestDriver, start), driverProxy));
-		componentId = thisIndex;
-  }
-
-	void connectVertex(const CCEdge & edge) {
-		adjlist.push_back(edge);
-	}
-
-  CCVertex(CkMigrateMessage *msg) {}
-
-	void start() {
-    ArrayMeshStreamer<dtype, int, CCVertex, SimpleMeshRouter>
-      * localAggregator = aggregator.ckLocalBranch();
-		// broadcast
-		typedef typename std::vector<CCEdge>::iterator Iterator; 
-		for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-#if defined (RADIUS)
-			localAggregator->insertData(dtype(componentId, R), it->v);
-#else
-			localAggregator->insertData(dtype(componentId), it->v);
-#endif
+	public:
+		CCVertex() : componentId(thisIndex) {
+			// Contribute to a reduction to signal the end of the setup phase
+			//contribute(CkCallback(CkReductionTarget(TestDriver, start), driverProxy));
 		}
-	}
 
-	inline void process(const dtype & m) {
-		const CmiUInt8 & c = m.c;
-#if defined (RADIUS)
-		const int & r = m.r;
-#endif
+		void connectVertex(const CCEdge & edge) {
+			if (edge.v > CkNumPes() * N) 
+				CkAbort("Incorrect dest vertex ID");
+			adjlist.push_back(edge);
+		}
 
-    ArrayMeshStreamer<dtype, int, CCVertex, SimpleMeshRouter>
-      * localAggregator = aggregator.ckLocalBranch();
+		void process(const CCEdge & edge) {
+			connectVertex(edge);
+		}
 
-		if (c < componentId) {
-			// update current weight and parent
-			componentId = c;
-			// broadcast
+		CCVertex(CkMigrateMessage *msg) {}
+
+		void start() {
+			ArrayMeshStreamer<dtype, long long, CCVertex, SimpleMeshRouter>
+				* localAggregator = aggregator.ckLocalBranch();
+
 			typedef typename std::vector<CCEdge>::iterator Iterator; 
 			for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-#if defined (RADIUS)
-				if (r > 0 )
-					localAggregator->insertData(dtype(componentId, r - 1), it->v);
-				else
-					thisProxy[it->v].update(dtype(componentId, R));
-#else
-				localAggregator->insertData(dtype(componentId), it->v);
-#endif
+				localAggregator->insertData(dtype(this->componentId, RADIX), it->v);
 			}
 		}
-  }
 
-	void update(const dtype & m) {
-		const CmiUInt8 & c = m.c;
-#if defined (RADIUS)
-		const int & r = m.r;
-#endif
-    ArrayMeshStreamer<dtype, int, CCVertex, SimpleMeshRouter>
-      * localAggregator = aggregator.ckLocalBranch();
+		void update(const CmiUInt8 & c, const int & r) {
+			if (c < componentId) {
+				//CkPrintf("%d (pe=%d): updated, radius %d\n", thisIndex, getuChareSet()->getPe(), r);
+				this->componentId = c;
+				if (r > 0) {
+					ArrayMeshStreamer<dtype, long long, CCVertex, SimpleMeshRouter>
+						* localAggregator = aggregator.ckLocalBranch();
+					typedef typename std::vector<CCEdge>::iterator Iterator; 
+					for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
+						localAggregator->insertData(dtype(this->componentId, r - 1), it->v);
+					}
+				} else {
+					typedef typename std::vector<CCEdge>::iterator Iterator; 
+					for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
+						thisProxy[it->v].update(this->componentId, RADIX);
+					}
+					//driverProxy.grayVertexExist();
+				}
+			}
+		}
 
-		if (c < componentId) {
-			// update current weight and parent
-			componentId = c;
-			// broadcast
+		void process(const dtype &data) {
+			update(data.c, data.r);
+		}
+
+		void getScannedVertexNum() {
+			CmiUInt8 c = 1/*(parent == -1 ? 0 : 1)*/;
+			contribute(sizeof(CmiUInt8), &c, CkReduction::sum_long,
+					CkCallback(CkReductionTarget(TestDriver, done), driverProxy));
+		}
+
+		void verify() {
 			typedef typename std::vector<CCEdge>::iterator Iterator; 
-			for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-#if defined (RADIUS)
-				if (r > 0 )
-					localAggregator->insertData(dtype(componentId, r - 1), it->v);
-				else
-					thisProxy[it->v].update(dtype(componentId, R));
-#else
-				localAggregator->insertData(dtype(componentId), it->v);
-#endif
-			}
+			for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) 
+				thisProxy[it->v].check(componentId);
 		}
-  }
 
-	void verify() {
-		typedef typename std::vector<CCEdge>::iterator Iterator; 
-		for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-			thisProxy[it->v].check(componentId);
+		void check(const CmiUInt8 & c) {
+			CkAssert(componentId == c);
 		}
-		//contribute(CkCallback(CkReductionTarget(TestDriver, done), driverProxy));
-	}
-
-	void check(CmiUInt8 c) {
-		if (componentId != c)
-			CkPrintf("%d: componentId incorrect componentId = %lld, c= %lld\n", thisIndex, componentId, c);
-		//CkAssert(componentId == c);
-	}
-
-	void print() {
-		std::stringstream ss;
-		typedef typename std::vector<CCEdge>::iterator Iterator; 
-		/*for (Iterator it = adjlist.begin(); it != adjlist.end(); it++) {
-			ss << '(' << it->v << ',' << it->w << ')';
-		}*/
-		CkPrintf("%d: %lld, %s\n", thisIndex, componentId, ss.str().c_str());
-	}
 };
 
-
-class TestDriver : public CBase_TestDriver {
-private:
-  CProxy_CCVertex  g;
-  double starttime;
-	Options opts;
-
-	CProxy_GraphGenerator<CProxy_CCVertex, CCEdge, Options> generator;
-
-public:
-  TestDriver(CkArgMsg* args) {
-		parseCommandOptions(args->argc, args->argv, opts);
-    driverProxy = thishandle;
-
-    // Create the chares storing vertices
-    g  = CProxy_CCVertex::ckNew(opts.N);
-		// create graph generator
-		generator = CProxy_GraphGenerator<CProxy_CCVertex, CCEdge, Options>::ckNew(g, opts); 
-
-    int dims[2] = {CkNumNodes(), CkNumPes() / CkNumNodes()};
-    CkPrintf("Aggregation topology: %d %d\n", dims[0], dims[1]);
-
-    // Instantiate communication library group with a handle to the client
-    aggregator =
-      CProxy_ArrayMeshStreamer<dtype, int, CCVertex, SimpleMeshRouter>
-      ::ckNew(numMsgsBuffered, 2, dims, g, 1);
-
-		CkStartQD(CkIndex_TestDriver::startGraphConstruction(), &thishandle);
-    delete args;
-  }
-
-  void startGraphConstruction() {
-		CkPrintf("CC/TRAM running...\n");
-		CkPrintf("\tnumber of mpi processes is %d\n", CkNumPes());
-		CkPrintf("\tgraph (s=%d, k=%d), scaling: %s\n", opts.scale, opts.K, (opts.strongscale) ? "strong" : "weak");
-		CkPrintf("Start graph construction:........\n");
-    starttime = CkWallTimer();
-
-		generator.generate();
-
-		CkStartQD(CkIndex_TestDriver::start(), &thishandle);
-	}
-
-
-  void start() {
-    double update_walltime = CkWallTimer() - starttime;
-		CkPrintf("Initialization completed:\n");
-    CkPrintf("CPU time used = %.6f seconds\n", update_walltime);
-    starttime = CkWallTimer();
-
-    CkCallback startCb(CkIndex_CCVertex::start(), g);
-    //CkCallback endCb(CkIndex_TestDriver::startVerificationPhase(), driverProxy);
-    CkCallback endCb(CkIndex_TestDriver::foo(), driverProxy);
-    aggregator.init(g.ckGetArrayID(), startCb, endCb, -1, true);
-
-		CkStartQD(CkIndex_TestDriver::startVerificationPhase(), &thishandle);
-  }
-
-  void startVerificationPhase() {
-		//aggregator.finish();
-		//aggregator.syncInit();
-		//aggregator.flushToIntermediateDestinations();
-		if (opts.verify) g.verify();
-		CkStartQD(CkIndex_TestDriver::done(), &thishandle);
-  }
-
-	void done() {
-		double update_walltime = CkWallTimer() - starttime;
-		//double gteps = 1e-9 * globalNubScannedVertices * 1.0/update_walltime;
-		CkPrintf("[Final] CPU time used = %.6f seconds\n", update_walltime);
-		//CkPrintf("Scanned vertices = %lld (%.0f%%)\n", globalNubScannedVertices, (double)globalNubScannedVertices*100/N);
-		//CkPrintf("%.9f Billion(10^9) Traversed edges  per second [GTEP/s]\n", gteps);
-		//CkPrintf("%.9f Billion(10^9) Traversed edges/PE per second [GTEP/s]\n",
-		//				 gteps / CkNumPes());
-
-		CkStartQD(CkIndex_TestDriver::exit(), &thishandle);
-	}
-
-	void exit() {
-		CkExit();
-	}
-
-	void checkErrors() {
-		//g.checkErrors();
-		//CkStartQD(CkIndex_TestDriver::reportErrors(), &thishandle);
-	}
-
-  void reportErrors(CmiInt8 globalNumErrors) {
-    //CkPrintf("Found %lld errors in %lld locations (%s).\n", globalNumErrors,
-    //         tableSize, globalNumErrors <= 0.01 * tableSize ?
-    //         "passed" : "failed");
-    CkExit();
-  }
-
-	void foo() {CkAbort("foo called");}
-};
-
+typedef GraphLib::GraphGenerator<
+	CCGraph, 
+	Options, 
+	GraphLib::VertexMapping::SingleVertex,
+	GraphLib::GraphType::Directed,
+	GraphLib::GraphGeneratorType::Kronecker,
+	GraphLib::TransportType::Tram> Generator;
+#include "driver.C"
 #include "tram_cc.def.h"
