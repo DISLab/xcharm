5e2319edf6b13746c05441d0220814aef7f2c96a
frolo <frolo@da144297-cd6b-48be-b417-ff744a2206ef>
2016-11-08 11:09:39 +0000

    old generator removed (Kronecker)

--- src/libs/graphrt/GraphGenerator.h
+++ src/libs/graphrt/GraphGenerator.h
@@ -19,73 +19,6 @@ namespace GraphLib {
 					 GraphGeneratorType graphGeneratorType, TransportType transportType>
 	class GraphGenerator;
 
-	// specification for Kronecker graph generator (Charm++) -- TODO: remove this template
-	template <typename Graph, typename Opts, VertexMapping vertexMapping>
-	class GraphGenerator<Graph, Opts, vertexMapping, GraphGeneratorType::Kronecker, TransportType::Charm> {
-		typedef CProxy_Generator_Charm<Graph, Opts, vertexMapping, GraphGeneratorType::Kronecker> CProxy_Generator; 
-		CProxy_Generator generator;
-		public:
-			GraphGenerator (Graph & g, Opts & opts) {
-				generator = CProxy_Generator::ckNew(g, opts);
-			}
-			void generate(const CkCallback & cb) { 
-				generator.generate(); 
-				CkStartQD(cb);
-			}
-	};
-
-	// specification for Kronecker graph generator (TRAM) -- TODO: remove this template
-	template <typename Graph, typename Opts>
-	class GraphGenerator<Graph, Opts, VertexMapping::SingleVertex, GraphGeneratorType::Kronecker, TransportType::Tram> {
-		typedef typename Graph::Proxy CProxy_Graph;
-		typedef typename Graph::Vertex Vertex;
-		typedef typename Graph::Edge Edge;
-		typedef CProxy_ArrayMeshStreamer<Edge, long long, Vertex, SimpleMeshRouter> CProxy_Aggregator;
-		typedef CProxy_Generator_Tram<Graph, Opts, VertexMapping::SingleVertex, GraphGeneratorType::Kronecker, CProxy_Aggregator> CProxy_Generator; 
-		typedef CkIndex_Generator_Tram<Graph, Opts, VertexMapping::SingleVertex, GraphGeneratorType::Kronecker, CProxy_Aggregator> CkIndex_Generator;
-		CProxy_Generator generator;
-		CProxy_Aggregator aggregator;
-		CProxy_Graph graphProxy;
-		Graph g;
-		public:
-			GraphGenerator (Graph & g, Opts & opts) : g(g), graphProxy(g.getProxy()) {
-				const int numMsgsBuffered = 1024;
-				int dims[2] = {CkNumNodes(), CkNumPes() / CkNumNodes()};
-				aggregator = CProxy_Aggregator::ckNew(numMsgsBuffered, 2, dims, graphProxy, 1);
-				generator = CProxy_Generator::ckNew(g /*graphProxy*/, opts, aggregator);
-			}
-			void generate() { 
-				CkCallback startCb(CkIndex_Generator::generate(), generator), fooCb;
-				aggregator.init(graphProxy.ckGetArrayID(), startCb, fooCb, -1, true);
-			}
-	};
-
-	// specification for Kronecker graph generator (TRAM, Multivertex) -- TODO: remove this template
-	template <typename Graph, typename Opts>
-	class GraphGenerator<Graph, Opts, VertexMapping::MultiVertex, GraphGeneratorType::Kronecker, TransportType::Tram> {
-		typedef typename Graph::Proxy CProxy_Graph;
-		typedef typename Graph::Vertex Vertex;
-		typedef typename Graph::Edge Edge;
-		typedef CProxy_ArrayMeshStreamer<std::pair<CmiUInt8, Edge>, long long, Vertex, SimpleMeshRouter> CProxy_Aggregator;
-		typedef CProxy_Generator_Tram<Graph, Opts, VertexMapping::MultiVertex, GraphGeneratorType::Kronecker, CProxy_Aggregator> CProxy_Generator; 
-		typedef CkIndex_Generator_Tram<Graph, Opts, VertexMapping::MultiVertex, GraphGeneratorType::Kronecker, CProxy_Aggregator> CkIndex_Generator;
-		CProxy_Generator generator;
-		CProxy_Aggregator aggregator;
-		CProxy_Graph graphProxy;
-		Graph g;
-		public:
-			GraphGenerator (Graph & g, Opts & opts) : g(g), graphProxy(g.getProxy()) {
-				const int numMsgsBuffered = 1024;
-				int dims[2] = {CkNumNodes(), CkNumPes() / CkNumNodes()};
-				aggregator = CProxy_Aggregator::ckNew(numMsgsBuffered, 2, dims, graphProxy, 1);
-				generator = CProxy_Generator::ckNew(g /*graphProxy*/, opts, aggregator);
-			}
-			void generate() { 
-				CkCallback startCb(CkIndex_Generator::generate(), generator), fooCb;
-				aggregator.init(graphProxy.ckGetArrayID(), startCb, fooCb, -1, true);
-			}
-	};
-
 	// specification for SSCA graph generator (Charm++)
 	template <typename Graph, typename Opts, VertexMapping vertexMapping> 
 	class GraphGenerator<Graph, Opts, vertexMapping, GraphGeneratorType::SSCA, TransportType::Charm> {
@@ -218,7 +151,6 @@ namespace GraphLib {
 
 
 }
-#include "OldGenerators.h"
 
 #define CK_TEMPLATES_ONLY
 #include "GraphGenerator.def.h"
